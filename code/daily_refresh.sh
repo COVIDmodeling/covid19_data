8aba95f156aa28652c79ef73e33b1f5310363cb1
mrazomej <manuel.razo.m@gmail.com>
2020-04-07 14:48:42 -0700

    update daily_refresh

--- code/daily_refresh.sh
+++ code/daily_refresh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(git rev-parse --show-toplevel)"
-cd code/processing/raw_data_processing
+cd code/
 python3 get_jhu_global_data.py
 python3 get_jhu_usa_data.py
 python3 get_nyt_usa_data.py
